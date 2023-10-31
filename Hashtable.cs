5e4b14d93e03ca26cbbc6520ec0d1d9dfe88958c
DevscopeTutorial <149447867+DevscopeTutorial@users.noreply.github.com>
2023-10-31 13:05:04 +1100

    Add files via upload

---
+++ Hashtable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+
+namespace HastableExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating Hashtable
+            Hashtable ht = new Hashtable();
+
+            //Adding Data
+            ht.Add("Fruit", "Apple, Orange, Banana");
+            ht.Add("Flower", "Lilly, Rose, Lotus");
+            ht.Add("Color","Red, Yellow, Blue");
+            DisplayAll(ht);
+
+            //Accessing a specific entry
+
+            Console.WriteLine("Accessing Fruit...");
+            Console.WriteLine(ht["Fruit"]);
+
+            //Updating hashtable
+
+            ht["Flower"] = "Rose, Tulip, Dahlia";
+            Console.WriteLine("After updating...");
+            DisplayAll(ht);
+
+            //Removing an element
+            Console.WriteLine("Removing color element...");
+            if(ht.ContainsKey("Color"))
+                ht.Remove("Color");
+            else
+                Console.WriteLine("Unknown key");
+            DisplayAll(ht);
+
+            //Size of the Hashtable
+
+            Console.WriteLine("Size of the hastable: " + ht.Count);
+
+            ht.Clear();
+            Console.WriteLine("Size of the hastable after clearing: " + ht.Count);
+
+
+        }
+
+        private static void DisplayAll(Hashtable ht)
+        {
+            foreach (DictionaryEntry entry in ht)
+            {
+                Console.WriteLine(entry.Key + " => " + entry.Value);
+            }
+        }
+    }
+}
---
+++ RomanToInteger.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace RomanToInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string roman = "MCMXCIV";
+            Hashtable ht = new Hashtable();
+
+            ht.Add("I", 1);
+            ht.Add("V", 5);
+            ht.Add("X", 10);
+            ht.Add("L", 50);
+            ht.Add("C", 100);
+            ht.Add("D", 500);
+            ht.Add("M", 1000);
+
+            int sum = 0;
+            for (int i=0; i<roman.Length; ++i) 
+            {
+                bool isCalculated = false;
+                switch (roman[i].ToString())
+                {
+                    case "I": 
+                        {
+                            if ((i + 1 < ht.Count) && (roman[i + 1]=='V' || roman[i + 1]=='X'))
+                            {
+                                sum -= Convert.ToInt32(ht[roman[i].ToString()]);
+                                isCalculated = true;
+                            }
+                            break; 
+                        }
+                    case "X": 
+                        {
+                            if ((i + 1 < ht.Count) && (roman[i + 1]=='L' || roman[i + 1]=='C'))
+                            {
+                                sum -= Convert.ToInt32(ht[roman[i].ToString()]);
+                                isCalculated = true;
+                            }
+                            break;
+                        }
+                    case "C":
+                        {
+                            if ((i + 1 < ht.Count) && (roman[i + 1]=='D' || roman[i + 1]=='M'))
+                            {
+                                sum -= Convert.ToInt32(ht[roman[i].ToString()]);
+                                isCalculated = true;
+                            }
+                            break;
+                        }
+
+                }
+                if(!isCalculated)
+                    sum += Convert.ToInt32(ht[roman[i].ToString()]);
+
+            }
+
+            Console.WriteLine(sum.ToString());
+            
+        }
+    }
+}
